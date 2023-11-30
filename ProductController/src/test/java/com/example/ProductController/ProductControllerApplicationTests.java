112219ea07b0eabd6c870866adfb9d98f4ad3731
sidharthgoutamsahu11@gmail.com <sidharth-goutam.sahu@capgemini.com>
2023-11-30 18:46:09 +0530

    hello

---
+++ ProductController/src/test/java/com/example/ProductController/ProductControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProductController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
